33b23f5556592268e69db531166db96aa7192f8a
Robert Wagner <rwwagner90@gmail.com>
2017-03-21 10:37:03 -0400

    Add shouldCompileJS workaround

--- index.js
+++ index.js
@@ -2,6 +2,16 @@
 
 module.exports = {
   name: 'lodash',
+  
+  /**
+   * Workaround needed for 2.12+
+   * see: https://github.com/ember-redux/ember-redux/issues/105#issuecomment-288001558
+   * @returns {boolean} Set to true to force JS compile
+   * @private
+   */
+  _shouldCompileJS: function () {
+    return true
+  },
 
   included (parent) {
     this._super.included(parent)
