12a9095155e30e84aede51881ff57a6736c2e286
Michael Carroll <juwara0@users.noreply.github.com>
2017-06-29 16:36:37 -0500

    upgrade to EmberCLI_2-12-3

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -4,7 +4,8 @@ const {Router: EmberRouter} = Ember
 import config from './config/environment'
 
 const Router = EmberRouter.extend({
-  location: config.locationType
+  location: config.locationType,
+  rootURL: config.rootURL
 })
 
 Router.map(function () {
